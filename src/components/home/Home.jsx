216985bfff63f528dcbc3ec509b362578b38b012
shnawaz844 <168626974+shnawaz844@users.noreply.github.com>
2024-07-30 15:34:48 +0530

    Intro and Intro carousel changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Calculator from "../calculator/Calculator";
 import { customersdata } from "../../components/data/data";
 import Customers from "../customers/Customers";
-import Intro from "../../components/intro/Intro";
+import Intro from "../intro/IntroCarousel.jsx";
 import Carousel from "../../components/carousel/Carousel";
 import Apply from "../../components/apply/Apply";
 import Advantages from "../../components/advantages/Advantages";
