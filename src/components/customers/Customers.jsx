e052a045b2df1799539773ee2b228b3ca9c72b82
Prashant <59646184+Prashantsayp@users.noreply.github.com>
2024-08-16 19:02:59 +0530

    testimonial-deign-change

--- src/components/customers/Customers.jsx
+++ src/components/customers/Customers.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Avatar, Container, Typography, Box, Paper } from "@mui/material";
+import { Avatar, Container, Typography, Box, Paper, Grid } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 import PropTypes from "prop-types";
 import API from "../../apis";
+
 const Customers = ({ customersdata }) => {
   const [customerRatings, setCustomerRatings] = useState([]);
+
   useEffect(() => {
     API.RatingRevAPI.getRating()
       .then((res) => {
@@ -20,7 +22,9 @@ const Customers = ({ customersdata }) => {
         console.log("Error fetching ratings:", err);
       });
   }, []);
+
   console.log("customerRatings", customerRatings);
+
   return (
     <Container
       maxWidth="false"
@@ -45,54 +49,110 @@ const Customers = ({ customersdata }) => {
       >
         What Our Customers Say
       </Typography>
+
       <Carousel height={"70vh"}>
         {customerRatings.map((customers, i) => (
-          <Paper
-            key={i}
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              boxShadow: 0,
-              padding: "20px",
-              margin: "20px",
-              cursor: "pointer",
-              "&:hover": {
-                boxShadow: 4,
-                transform: "scale(1.02)",
-              },
-            }}
-          >
-            <Avatar
-              src={customers.image_url} // Assuming image_url would be provided
-              sx={{ height: "150px", width: "150px", marginBottom: "20px" }}
-            />
-            <Typography
-              variant="h3"
-              sx={{
-                wordWrap: "break-word",
-                width: "70%",
-                lineHeight: "2rem",
-                textAlign: "center",
-                marginBottom: "20px",
-              }}
-            >
-              {customers.review}
-            </Typography>
-            <Typography sx={{ color: "purple", fontSize: "20px" }}>
-              {customers.name}
-            </Typography>
-            <Typography sx={{ color: "blue" }}>
-              {customers.city || "City not available"},{" "}
-              {customers.state || "State not available"}
-            </Typography>
-          </Paper>
+          <Grid container spacing={2} key={i}>
+            <Grid item xs={12} md={6}>
+              <Paper
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  boxShadow: 0,
+                  padding: "20px",
+                  margin: "20px",
+                  cursor: "pointer",
+                  "&:hover": {
+                    boxShadow: 4,
+                    transform: "scale(1.02)",
+                  },
+                }}
+              >
+                <Avatar
+                  src={customers.image_url} // Assuming image_url would be provided
+                  sx={{
+                    height: "150px",
+                    width: "150px",
+                    marginBottom: "20px",
+                  }}
+                />
+                <Typography
+                  variant="h3"
+                  sx={{
+                    wordWrap: "break-word",
+                    width: "70%",
+                    lineHeight: "2rem",
+                    textAlign: "center",
+                    marginBottom: "20px",
+                  }}
+                >
+                  {customers.review}
+                </Typography>
+                <Typography sx={{ color: "purple", fontSize: "20px" }}>
+                  {customers.name}
+                </Typography>
+                <Typography sx={{ color: "blue" }}>
+                  {customers.city || "City not available"},{" "}
+                  {customers.state || "State not available"}
+                </Typography>
+              </Paper>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <Paper
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  boxShadow: 0,
+                  padding: "20px",
+                  margin: "20px",
+                  cursor: "pointer",
+                  "&:hover": {
+                    boxShadow: 4,
+                    transform: "scale(1.02)",
+                  },
+                }}
+              >
+                <Avatar
+                  src={customers.image_url} // Assuming image_url would be provided
+                  sx={{
+                    height: "150px",
+                    width: "150px",
+                    marginBottom: "20px",
+                  }}
+                />
+                <Typography
+                  variant="h3"
+                  sx={{
+                    wordWrap: "break-word",
+                    width: "70%",
+                    lineHeight: "2rem",
+                    textAlign: "center",
+                    marginBottom: "20px",
+                  }}
+                >
+                  {customers.review}
+                </Typography>
+                <Typography sx={{ color: "purple", fontSize: "20px" }}>
+                  {customers.name}
+                </Typography>
+                <Typography sx={{ color: "blue" }}>
+                  {customers.city || "City not available"},{" "}
+                  {customers.state || "State not available"}
+                </Typography>
+              </Paper>
+            </Grid>
+          </Grid>
         ))}
       </Carousel>
     </Container>
   );
 };
+
 Customers.propTypes = {
   customersdata: PropTypes.arrayOf(
     PropTypes.shape({
@@ -103,4 +163,5 @@ Customers.propTypes = {
     })
   ).isRequired,
 };
+
 export default Customers;
